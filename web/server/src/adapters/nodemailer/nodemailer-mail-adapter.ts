4dffee6458ce6746e2401a2e29badb11a8a60b36
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-05-08 16:56:01 -0300

    feat: backend

---
+++ web/server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "5fe08cb20742b3",
+      pass: "494bd930cefcc0"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Anderson André <andreandersoncaue.e@gmail.com>',
+            subject,
+            html: body,
+        })
+    };
+}
